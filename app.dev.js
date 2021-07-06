b21bdc5b5f1dce388c38bdf8456df6d78f1dd4c4
Shalini Nangia <shalininangia@Shalinis-MacBook-Pro.local>
2021-07-06 21:07:39 +0100

    re-write code cleaner

--- app.dev.js
+++ app.dev.js
@@ -1,79 +1,133 @@
 "use strict";
 
-//Variables
-//const startPage = document.querySelector('.start__page');
-var startBtn = document.querySelector('.start__button');
-var optionBtn = document.querySelectorAll('.options button');
-var questionText = document.querySelector('.question__h2');
-var questionArea = document.querySelector('.question');
-var optionsClick = document.querySelector('.options'); // Start button
-
+var startBtn = document.querySelector('.start__btn');
+var quizArea = document.querySelector('.quiz__area');
 startBtn.addEventListener('click', function (e) {
-  startBtn.classList.add('dis-none');
-  questionArea.classList.remove('dis-none');
-  optionsClick.classList.remove('dis-none');
-}); //Quiz questions
-
-var quiz = [{
+  startBtn.classList.add('display__none');
+  quizArea.classList.remove('display__none');
+});
+var questions = [{
   question: 'Which type of beans are used to make baked beans?',
-  options: ['Cannellini beans', 'Borlotti beans', 'Haricot beans'],
-  answer: 'Haricot beans'
+  answers: ['Cannellini beans', 'Borlotti beans', 'Haricot beans'],
+  correct: 'Haricot beans'
 }, {
   question: 'Which flower does the spice saffron come from?',
-  options: ['Lily', 'Crocus', 'Orchid'],
-  answer: 'Crocus'
+  answers: ['Lily', 'Crocus', 'Orchid'],
+  correct: 'Crocus'
 }, {
   question: 'Why is white chocolate white?',
-  options: ['It is mostly milk', 'It has no cocoa solids', 'The colour has been bleached out'],
-  answer: 'It has no cocoa solids'
+  answers: ['It is mostly milk', 'It has no cocoa solids', 'The colour has been bleached out'],
+  correct: 'It has no cocoa solids'
 }, {
   question: 'What can chocolate cause?',
-  options: ['Acne', 'Migraines', 'Both'],
-  answer: 'Migraines'
+  answers: ['Acne', 'Migraines', 'Both'],
+  correct: 'Migraines'
 }, {
   question: 'Which vegetable is a flower you can eat?',
-  options: ['Broccoli', 'Lettuce', 'Squash'],
-  answer: 'Broccoli'
+  answers: ['Broccoli', 'Lettuce', 'Squash'],
+  correct: 'Broccoli'
 }, {
   question: 'What vegetable has varieties known as Bell Tower, Orobelle, and Jupiter?',
-  options: ['Onion', 'Pepper', 'Squash'],
-  answer: 'Pepper'
+  answers: ['Onion', 'Pepper', 'Squash'],
+  correct: 'Pepper'
 }, {
   question: 'Where in the world did wiener schnitzel come from?',
-  options: ['Italy', 'Austria', 'Germany'],
-  answer: 'Austria'
+  answers: ['Italy', 'Austria', 'Germany'],
+  correct: 'Austria'
 }, {
   question: 'Natural-made honey is:',
-  options: ['Bee poop', 'Bee vomit', 'Bee urine'],
-  answer: 'Bee vomit'
+  answers: ['Bee poop', 'Bee vomit', 'Bee urine'],
+  correct: 'Bee vomit'
 }, {
   question: 'Chinese gooseberry is another name for:',
-  options: ['Kiwi', 'Cherry', 'Plum'],
-  answer: 'Kiwi'
+  answers: ['Kiwi', 'Cherry', 'Plum'],
+  correct: 'Kiwi'
 }, {
   question: 'What percentage of the watermelon is water?',
-  options: ['45 percent', '66 percent', '92 percent'],
-  answer: '92 percent'
-}]; //Math.random() to select random questions 
+  answers: ['45 percent', '66 percent', '92 percent'],
+  correct: '92 percent'
+}]; // Requirements
+// 1. Dynamic HTML
+// 1.1 Change number
+// 1.2 Change question
+// 1.3 Change answers
+// 2. Change styles of buttons on click
+// 3. Make sure we get the correct answer when we do click on the
+// correct one
+// 4. Reset stylings when we move on
+// 5. Randomisation
 
-var random = Math.random();
-var roundNumber = Math.round(random * 10); // Display the question in h2 tag 
-
-questionText.innerHTML = quiz[roundNumber].question; // Display 3 answers inside 3 buttons
-
-optionBtn[0].innerHTML += quiz[roundNumber].options[0];
-optionBtn[1].innerHTML += quiz[roundNumber].options[1];
-optionBtn[2].innerHTML += quiz[roundNumber].options[2]; //click event answer buttons, if statement score
-
-var score = 0;
-optionBtn.forEach(function (button) {
-  button.addEventListener('click', function (e) {
-    if (e.target.innerHTML === quiz[roundNumber].answer) {
-      score++;
-      console.log(score);
-    } else if (e.target.innerHTML !== quiz[roundNumber].answer) {
-      score = 0;
-      console.log(score);
-    }
+var shuffle = function shuffle(arr) {
+  return arr.sort(function () {
+    return .5 - Math.random();
   });
-});
+};
+
+shuffle(questions);
+
+var attachCheck = function attachCheck() {
+  var buttons = document.querySelectorAll(".answer");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      console.log(event.target.innerHTML);
+      console.log(event);
+      var answer = event.target.innerHTML;
+      checkCorrectAnswer(answer);
+    });
+  });
+}; //question check - question 1 of 10, 2 of 10
+
+
+var pageNumber = 0;
+
+var incrementPage = function incrementPage() {
+  var questionCheck = document.querySelector('.question-check');
+  pageNumber++;
+  questionCheck.innerHTML = "Question ".concat(pageNumber, " of 10");
+}; // Generate dynamic HTML
+
+
+var generateHTML = function generateHTML(question, answer1, answer2, answer3) {
+  // Get the main quiz area
+  var main = document.querySelector(".quiz-area"); // Reset to empty
+
+  main.innerHTML = ""; // Add in the stuff I want
+
+  main.innerHTML = "\n    <h2>".concat(question, "</h2>\n    <section>\n      <button class=\"answer\">").concat(answer1, "</button>\n      <button class=\"answer\">").concat(answer2, "</button>\n      <button class=\"answer\">").concat(answer3, "</button>\n    </section>\n  ");
+  attachCheck();
+}; // start at the first question (index 0)
+// When click next - increment the number
+// Generate HTML again
+
+
+var questionNumber = 0;
+
+var incrementQuestion = function incrementQuestion() {
+  questionNumber++;
+  var question = questions[questionNumber];
+  generateHTML(question.question, question.answers[0], question.answers[1], question.answers[2]);
+};
+
+var question = questions[questionNumber];
+generateHTML(question.question, question.answers[0], question.answers[1], question.answers[2]);
+document.querySelector(".next").addEventListener("click", function () {
+  incrementQuestion();
+  incrementPage();
+}); // Set score to zero at the beginning of the game 
+
+var score = 0; // When I click the button
+
+var checkCorrectAnswer = function checkCorrectAnswer(answer) {
+  // Check it against the correct answer
+  var question = questions[questionNumber]; // IF answer is correct 
+
+  if (question.correct === answer) {
+    // When click the correct answer, score 1
+    score++;
+    console.log(score);
+  } else {
+    // When click the incorrect answer, score 0
+    score = 0;
+    console.log(score);
+  }
+};
--- app.js
+++ app.js
@@ -1,100 +1,163 @@
-//Variables
-//const startPage = document.querySelector('.start__page');
-const startBtn = document.querySelector('.start__button');
-const optionBtn = document.querySelectorAll('.options button');
-const questionText = document.querySelector('.question__h2');
-const questionArea = document.querySelector('.question')
-const optionsClick = document.querySelector('.options');
+const startBtn = document.querySelector('.start__btn');
+const quizArea = document.querySelector('.quiz__area');
 
 
-// Start button
-startBtn.addEventListener('click', e => {
-  startBtn.classList.add('dis-none');
-  questionArea.classList.remove('dis-none');
-  optionsClick.classList.remove('dis-none');
+startBtn.addEventListener('click', (e) => {
+  startBtn.classList.add('display__none');
+  quizArea.classList.remove('display__none');
 })
 
-//Quiz questions
-const quiz = [
+
+const questions = [ 
   {
     question: 'Which type of beans are used to make baked beans?',
-    options: ['Cannellini beans', 'Borlotti beans', 'Haricot beans'],
-    answer: 'Haricot beans' 
+    answers: ['Cannellini beans', 'Borlotti beans', 'Haricot beans'],
+    correct: 'Haricot beans' 
   },
   {
     question: 'Which flower does the spice saffron come from?',
-    options: ['Lily', 'Crocus', 'Orchid'],
-    answer: 'Crocus'
+    answers: ['Lily', 'Crocus', 'Orchid'],
+    correct: 'Crocus'
   },
   {
     question: 'Why is white chocolate white?',
-    options: ['It is mostly milk', 'It has no cocoa solids', 'The colour has been bleached out'],
-    answer: 'It has no cocoa solids'
+    answers: ['It is mostly milk', 'It has no cocoa solids', 'The colour has been bleached out'],
+    correct: 'It has no cocoa solids'
   },
   {
     question: 'What can chocolate cause?',
-    options: ['Acne', 'Migraines', 'Both'],
-    answer: 'Migraines'
+    answers: ['Acne', 'Migraines', 'Both'],
+    correct: 'Migraines'
   },
   {
     question: 'Which vegetable is a flower you can eat?',
-    options: ['Broccoli', 'Lettuce', 'Squash'],
-    answer: 'Broccoli'
+    answers: ['Broccoli', 'Lettuce', 'Squash'],
+    correct: 'Broccoli'
   },
   {
     question: 'What vegetable has varieties known as Bell Tower, Orobelle, and Jupiter?',
-    options: ['Onion', 'Pepper', 'Squash'],
-    answer: 'Pepper'
+    answers: ['Onion', 'Pepper', 'Squash'],
+    correct: 'Pepper'
   },
   {
     question: 'Where in the world did wiener schnitzel come from?',
-    options: ['Italy',  'Austria', 'Germany'],
-    answer: 'Austria'
+    answers: ['Italy',  'Austria', 'Germany'],
+    correct: 'Austria'
   },
   {
     question: 'Natural-made honey is:',
-    options: ['Bee poop', 'Bee vomit', 'Bee urine'],
-    answer: 'Bee vomit'
+    answers: ['Bee poop', 'Bee vomit', 'Bee urine'],
+    correct: 'Bee vomit'
   },
   {
     question: 'Chinese gooseberry is another name for:',
-    options: ['Kiwi', 'Cherry', 'Plum'],
-    answer: 'Kiwi'
+    answers: ['Kiwi', 'Cherry', 'Plum'],
+    correct: 'Kiwi'
   },
   {
     question: 'What percentage of the watermelon is water?',
-    options: ['45 percent', '66 percent', '92 percent'],
-    answer: '92 percent'
+    answers: ['45 percent', '66 percent', '92 percent'],
+    correct: '92 percent'
   }
-] 
+];
 
-//Math.random() to select random questions 
-const random = Math.random();
-const roundNumber = Math.round(random * 10);
+// Requirements
+// 1. Dynamic HTML
+// 1.1 Change number
+// 1.2 Change question
+// 1.3 Change answers
+// 2. Change styles of buttons on click
+// 3. Make sure we get the correct answer when we do click on the
+// correct one
+// 4. Reset stylings when we move on
+// 5. Randomisation
 
-// Display the question in h2 tag 
-questionText.innerHTML = quiz[roundNumber].question;
+const shuffle = arr => arr.sort(() => .5 - Math.random());
+shuffle(questions);
 
-// Display 3 answers inside 3 buttons
-optionBtn[0].innerHTML += quiz[roundNumber].options[0];
-optionBtn[1].innerHTML += quiz[roundNumber].options[1];
-optionBtn[2].innerHTML += quiz[roundNumber].options[2]; 
+const attachCheck = () => {
+  const buttons = document.querySelectorAll(".answer");
+  buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+      console.log(event.target.innerHTML)
+      console.log(event)
+      const answer = event.target.innerHTML;
+      checkCorrectAnswer(answer);
+    })
+  })
+}
 
-//click event answer buttons, if statement score
+//question check - question 1 of 10, 2 of 10
 
-let score = 0;
+let pageNumber = 0;
+const incrementPage = () => {
+  const questionCheck = document.querySelector('.question-check');
+  pageNumber++ 
+  questionCheck.innerHTML = `Question ${pageNumber} of 10`;
+}
 
- optionBtn.forEach(button => {
-   button.addEventListener('click', e => {
-     if(e.target.innerHTML === quiz[roundNumber].answer){
-      score ++;
-      console.log(score);
-     }else if (e.target.innerHTML !== quiz[roundNumber].answer){
-       score = 0;
-       console.log(score);
-     }
- })  
+// Generate dynamic HTML
+const generateHTML = (question, answer1, answer2, answer3) => {
+  // Get the main quiz area
+  const main = document.querySelector(".quiz-area");
+  // Reset to empty
+  main.innerHTML = "";
+  // Add in the stuff I want
+  main.innerHTML = `
+    <h2>${question}</h2>
+    <section>
+      <button class="answer">${answer1}</button>
+      <button class="answer">${answer2}</button>
+      <button class="answer">${answer3}</button>
+    </section>
+  `;
+  attachCheck();
+};
+// start at the first question (index 0)
+// When click next - increment the number
+// Generate HTML again
+let questionNumber = 0;
+const incrementQuestion = () => {
+  questionNumber++;
+  const question = questions[questionNumber];
+  generateHTML(
+    question.question,
+    question.answers[0],
+    question.answers[1],
+    question.answers[2],
+  );
+};
+
+const question = questions[questionNumber];
+generateHTML(
+  question.question,
+  question.answers[0],
+  question.answers[1],
+  question.answers[2],
+);
+document.querySelector(".next").addEventListener("click", () => {
+  incrementQuestion();
+  incrementPage();
 })
+
+
+// Set score to zero at the beginning of the game 
+let score = 0;
+// When I click the button
+const checkCorrectAnswer = (answer) => {
+  // Check it against the correct answer
+  const question = questions[questionNumber]
+  // IF answer is correct 
+  if (question.correct === answer) {
+    // When click the correct answer, score 1
+    score++
+    console.log(score);
+  } else {
+    // When click the incorrect answer, score 0
+    score = 0
+    console.log(score);
+  }
+}
 
 
 
